9c93cd5da6d6e38303090c19636ec4476bdd2cda
cgint <christian.gintenreiter@gmail.com>
2024-04-06 19:06:00 +0200

    add groq_connect

--- groq_connect.py
+++ groq_connect.py
@@ -1,20 +1,22 @@
-from langchain_groq import Groq
+from langchain_groq import ChatGroq
 
 def get_llm_groq(model_name: str, temperature: float, max_tokens: int):
-    return Groq(model=model_name, temperature=temperature, max_tokens=max_tokens)
+    return ChatGroq(model_name=model_name, temperature=temperature, max_tokens=max_tokens)
 
-model_openhermes = "teknium/OpenHermes-2-Mistral-7B"
-model_mixtral_8x7B = "mistralai/Mixtral-8x7B-v0.1"
-model_mixtral_8x7B_instruct = "mistralai/Mixtral-8x7B-Instruct-v0.1"
-model_mistral_7B_instruct = "mistralai/Mistral-7B-Instruct-v0.2"
+model_llama2_70b = "llama2-70b-4096"
+model_mixtral_8x7B_instruct = "mixtral-8x7b-32768"
+model_gemma_7B_instruct = "gemma-7b-it"
 
-def llm_list_groq() -> list[str]:
+def get_models_groq() -> list[str]:
     return [
-        model_openhermes,
         model_mixtral_8x7B_instruct,
-        model_mistral_7B_instruct,
+        model_gemma_7B_instruct,
+        model_llama2_70b
     ]
 
 def get_model_default_groq() -> str:
+    return model_llama2_70b
+
+def get_model_tools_groq() -> str:
     return model_mixtral_8x7B_instruct
 
